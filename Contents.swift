6c42487f8935119f2c94da605af4e94e818830c3
Matt Powley <mattpow35@gmail.com>
2016-09-21 08:09:38 -0600

    created 3 UIview boxes

---
+++ Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var DynamicView = UIView(frame: CGRectMake(100, 200, 300, 300))
+
+DynamicView.backgroundColor=UIColor.greenColor()
+DynamicView.layer.cornerRadius=30
+DynamicView.layer.borderWidth=20
+DynamicView
+
+var Star = UIView(frame: CGRectMake(200, 300, 400, 400))
+Star.backgroundColor=UIColor.blueColor()
+Star.layer.cornerRadius=400
+Star
+
+var UIbox = UIView(frame: CGRectMake(300, 300, 300, 300))
+UIbox.backgroundColor=UIColor.purpleColor()
+UIbox.layer.borderWidth = 50
+UIbox.layer.cornerRadius = 30
+UIbox
+
+
+
+
+
